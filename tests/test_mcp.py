ba424b9d978eccea1f90008aab04518e7f12972a
Christopher T Earley <jrekct@gmail.com>
2025-05-14 17:10:11 -0600

    adding todos

--- tests/test_mcp.py
+++ tests/test_mcp.py
@@ -16,3 +16,10 @@ async def test_mcp_client_lists_tools():
             response = await session.list_tools()
             tools = response.tools
             assert tools, "Expected at least one tool from the MCP server"
+
+             
+#TODO: Add tools that take prompts and test that the correct tool(s) are selected
+#TODO: Find way to only create client once per test module or make it faster
+#TODO: Add LLM to client 
+##TODO: Make LLM easily configurable
+##TODO: Make it so we can test against multiple LLMs
